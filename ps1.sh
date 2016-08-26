69034d432eb8b32dfc737e6db80a7ea621940db8
Brent Roose <brent.roose@gmail.com>
2016-08-26 16:33:05 +0200

    Update PS1

--- ps1.sh
+++ ps1.sh
@@ -1,13 +1,27 @@
-alias __git_ps1="git branch 2>/dev/null | grep '*' | sed 's/* \(.*\)/(\1)/'"
-function get_git_color {
+function git_ps1 {
     if [[ -n $(git status --porcelain 2>/dev/null) ]]
     then
-        echo -e $red
+        color=$red
     else
-        echo -e $green
+        color=$green
+    fi
+
+    branch=$(git branch 2>/dev/null | grep '*' | sed 's/* \(.*\)/\1/')
+    tag=$(version)
+
+    if [ $branch ]
+    then
+        if [ $tag == "0.0.0" ]
+        then
+            echo -e "${color}(${branch}) "
+        else
+            echo -e "${color}(${branch} @ ${tag}) "
+        fi
     fi
 }
-PS1="${bold_cyan}(\h)${reset} \u ${bold_cyan}\w\$(get_git_color) \$(__git_ps1)${reset} "
+
+PS1="${bold_cyan}(\h)${reset} \u ${bold_cyan}\w \$(git_ps1)${reset}"
+# PS1="${bold_cyan}(\h)${reset} \u ${bold_cyan}\w\$(get_git_color) \$(__git_ps1)${reset} "
 
 export CLICOLOR=1
 export LSCOLORS=cxBxDxCxegedabagacaC
